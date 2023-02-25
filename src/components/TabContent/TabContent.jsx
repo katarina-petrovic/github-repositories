a6399aa07edc55c6c97ce4cb629e154864448a38
Katarina <k.petrovic90@gmail.com>
2023-02-25 21:51:23 +0100

    Added pagination component and implemented on tab content

--- src/components/TabContent/TabContent.jsx
+++ src/components/TabContent/TabContent.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import useRepositories from "../../hooks/useRepositories";
 import Loader from "../Loader/Loader";
 import RepositoryList from "../RepositoryList/RepositoryList";
+import Pagination from "../Pagination/Pagination";
 import { TabsNavigationContext } from "../../providers/TabsProvider/TabsNavigationProvider";
 
 const ITEMS_PER_PAGE = 12;
@@ -9,17 +10,30 @@ const ITEMS_PER_PAGE = 12;
 const TabContent = () => {
 
     const [currentSortType, setCurrentSortType] = useState("stars");
-
+    const [currentPage, setCurrentPage] = useState(1);
 
     const { activeTab } = useContext(TabsNavigationContext);
     const { isLoading, data, error } = useRepositories({
         topic: activeTab,
         sort: currentSortType,
         itemsPerPage: ITEMS_PER_PAGE,
-        //currentPage: currentPage
-        currentPage: 1
-
+        currentPage: currentPage
     });
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+    const previousPage = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const nextPage = () => {
+        if (currentPage !== Math.ceil(100 / ITEMS_PER_PAGE)) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
 
     if (isLoading) return <Loader />;
     if (error) return "try again";
@@ -27,6 +41,13 @@ const TabContent = () => {
     return (
         <div className="tab-content">
             <RepositoryList items={data.items} />
+            <Pagination
+                postsPerPage={ITEMS_PER_PAGE}
+                totalPosts={100}
+                paginate={paginate}
+                previousPage={previousPage}
+                nextPage={nextPage}
+            />
         </div>
     )
 
